7ef89a569560f54fa99831aa10fdd3b9c0d71d71
Medard Urban <umedard@gmail.com>
2019-06-25 19:44:12 +0700

    change form

--- src/components/mailchimp/Mail.tsx
+++ src/components/mailchimp/Mail.tsx
@@ -15,23 +15,40 @@ export const Mail = () => {
             </div>
          
 
-          3rd form
-
-          <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
-            <p class="hidden">
-              <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
-            </p>
-            <p>
-              <label>Email: <input type="text" name="email" className={s.input}  /></label>
-            </p>
-            <p>
-              <label>Name: <input type="text" name="text" className={s.input}  /></label>
-            </p>
           
-            <p>
-              <button type="submit" className={s.input} >Send</button>
-            </p>
-          </form>
+
+            <form
+                name="contact"
+                method="post"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+              >
+                <input type="hidden" name="bot-field" />
+                <input type="hidden" name="form-name" value="contact" />
+                
+
+
+                <div className="field half first">
+                  <label htmlFor="name">Name</label>
+                  <input type="text" name="name" id="name" />
+                </div>
+                <div className="field half">
+                  <label htmlFor="email">Email</label>
+                  <input type="text" name="email" id="email" />
+                </div>
+                <div className="field">
+                  <label htmlFor="message">Message</label>
+                  <textarea name="message" id="message" rows="6" />
+                </div>
+                <ul className="actions">
+                  <li>
+                    <input type="submit" value="Send Message" className="special" />
+                  </li>
+                  <li>
+                    <input type="reset" value="Clear" />
+                  </li>
+                </ul>
+              </form>
             
         </Container>
      
