842ba03f90cf986cd2bdd347750ed6c5ce30941e
Medard Urban <umedard@gmail.com>
2019-06-25 18:35:17 +0700

    add 2nd form

--- src/components/mailchimp/Mail.tsx
+++ src/components/mailchimp/Mail.tsx
@@ -30,6 +30,20 @@ export const Mail = () => {
                 
               </ul>
             </form>
+
+            Form 2
+
+            <form name="contact" netlify>
+            <p>
+              <label>Name <input type="text" name="name" /></label>
+            </p>
+            <p>
+              <label>Email <input type="email" name="email" /></label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
             
         </Container>
      
