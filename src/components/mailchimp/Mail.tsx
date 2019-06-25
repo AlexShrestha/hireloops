7931a1f84c679fe33163178c93bb08f3e5fc803d
Medard Urban <umedard@gmail.com>
2019-06-25 18:32:11 +0700

    add form

--- src/components/mailchimp/Mail.tsx
+++ src/components/mailchimp/Mail.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import s from './Mail.scss';
 
 import { Container } from '../container/Container';
-import { Row } from '../row/Row';
+
 
 export const Mail = () => {
     return (
@@ -13,7 +13,23 @@ export const Mail = () => {
               </p> 
               
             </div>
-          
+            <form method="post" action="#">
+              <div className="field half first">
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="field half">
+                <label htmlFor="email">Email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              
+              <ul className="actions">
+                <li>
+                  <input type="submit" value="Send Message" className="special" />
+                </li>
+                
+              </ul>
+            </form>
             
         </Container>
      
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,33 @@ import { Intro } from 'components/intro/Intro';
 import { BlockText } from 'components/block-text/BlockText';
 import { Mail } from 'components/mailchimp/Mail';
 
+import { Header } from 'components/header/Header';
+import { Link as HeaderLink } from 'components/header/Link';
+import { Footer } from 'components/footer/Footer';
+
+import Logo from 'assets/svg/ueno-logo.svg';
+import Dribbble from 'assets/svg/dribbble.svg';
+import Twitter from 'assets/svg/twitter.svg';
+import Github from 'assets/svg/github.svg';
+import Instagram from 'assets/svg/instagram.svg';
+import Facebook from 'assets/svg/facebook.svg';
+import Linkedin from 'assets/svg/linkedin.svg';
+
 export default () => (
   <>
     <Helmet title="Loops" />
+    <Header>
+      {/* <HeaderLink
+        name="about"
+        to="/about"
+      />
 
+      <HeaderLink
+        name="github"
+        to="https://github.com/ueno-llc"
+        icon={<Github />}
+      /> */}
+    </Header>
     <Intro>
     We build ROI driven campaigns that grow your business. Whether it’s Facebook, Instagram, Google, Social Video Marketing, Email Marketing, User Generated Content. We help you find, pick and implement the most impactful strategies that drive revenue. </Intro>
     <Mail />
@@ -22,6 +45,18 @@ In the past 7 years, we have been honored to work with amazing companies like Su
 
         </>
       )}
+    />
+
+<Footer
+      logo={<Logo />}
+      social={[
+        { icon: <Dribbble />, to: 'https://dribbble.com/ueno' },
+        { icon: <Twitter />, to: 'https://twitter.com/uenodotco' },
+        { icon: <Github />, to: 'https://github.com/ueno-llc' },
+        { icon: <Instagram />, to: 'https://www.instagram.com/uenodotco' },
+        { icon: <Facebook />, to: 'https://www.facebook.com/uenodotco' },
+        { icon: <Linkedin />, to: 'https://www.linkedin.com/company/ueno' },
+      ]}
     />
   </>
 );
