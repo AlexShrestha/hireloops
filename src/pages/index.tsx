f530cc2041a377c9948064c89af77a9208b35db1
Medard Urban <umedard@gmail.com>
2019-06-26 21:37:42 +0700

    remove comma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,12 +38,8 @@ export default () => (
     </Header>
     <Intro>
     We build ROI driven campaigns that grow your business. 
-    Whether it’s <Highlight>Facebook</Highlight>, <Highlight>Instagram</Highlight>, <Highlight>Google</Highlight>,  <Highlight>Social</Highlight> <Highlight>Video </Highlight> <Highlight>Marketing</Highlight>, <Highlight>Email </Highlight> <Highlight>Marketing </Highlight>, 
+    Whether it’s <Highlight>Facebook</Highlight>, <Highlight>Instagram</Highlight>, <Highlight>Google</Highlight>,  <Highlight>Social</Highlight> <Highlight>Video </Highlight> <Highlight>Marketing</Highlight>, <Highlight>Email </Highlight> <Highlight>Marketing </Highlight>, <Highlight>User</Highlight> <Highlight>Generated </Highlight> <Highlight> Content</Highlight>. We help you find, pick and implement the most impactful strategies that drive revenue. 
 
-<Highlight>User</Highlight> <Highlight>Generated </Highlight> <Highlight> Content</Highlight>. We help you find, pick and implement the most impactful strategies that drive revenue. 
-    
-  
-    
     <Mail />
     </Intro>
       
