93c561fb7387f700f7b482d3245a681e0e22c98b
Medard Urban <umedard@gmail.com>
2019-06-26 21:03:23 +0700

    highlight

--- src/components/intro/Text.tsx
+++ src/components/intro/Text.tsx
@@ -7,7 +7,7 @@ export const Text = () => {
   return (
     <div>
     We build ROI driven campaigns that grow your business. 
-    Whether it’s <Highlight>Facebook</Highlight>, <Highlight>Instagram</Highlight>, <Highlight>Google</Highlight>,  <Highlight>Social</Highlight> <Highlight>Video </Highlight><Highlight>Marketing</Highlight>, <Highlight>Email Marketing</Highlight>,   
+    Whether it’s <Highlight>Facebook</Highlight>, <Highlight>Instagram</Highlight>, <Highlight>Google</Highlight>,  <Highlight>Social</Highlight> <Highlight>Video </Highlight><Highlight>Marketing</Highlight>, <Highlight>Email </Highlight><Highlight>Marketing</Highlight>,   
      <Highlight>User</Highlight> <Highlight>Generated </Highlight><Highlight>Content</Highlight>. We help you find, pick and implement the most impactful strategies that drive revenue. 
     
 
